42002af3973efdedbbab70a182639cd79d1bd2e5
Michael Koka <tikki-mikk@yandex.ru>
2019-02-06 16:38:13 +0200

    Initial Commit

---
+++ G68L4DZ/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  G68L4DZ
+//
+//  Created by Misha on 06/02/2019.
+//  Copyright © 2019 Misha. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
